69c38b7475d88ee4cbb7bf93d0c887617a07f825
koshelvadim <koshelvadim@mail.ru>
2022-01-31 14:49:21 +0300

    lesson7

--- lesson1/src/plagins/modal/index.js
+++ lesson1/src/plagins/modal/index.js
@@ -2,6 +2,7 @@ export default {
   install(Vue) {
     if (this.installed) return;
     this.installed = true;
+    // eslint-disable-next-line no-param-reassign
     Vue.prototype.$modal = {
       EventBus: new Vue(),
       show(settings) {
